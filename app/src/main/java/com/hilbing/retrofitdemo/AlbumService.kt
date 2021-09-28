87620c42b396f45d580f6bb643397d919300f31b
Gretel Hilbing <ghilbing@gmail.com>
2021-09-27 18:57:23 -0700

    Logging Interceptor

--- app/src/main/java/com/hilbing/retrofitdemo/AlbumService.kt
+++ app/src/main/java/com/hilbing/retrofitdemo/AlbumService.kt
@@ -2,6 +2,7 @@ package com.hilbing.retrofitdemo
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface AlbumService {
@@ -10,4 +11,7 @@ interface AlbumService {
 
     @GET("/albums")
     suspend fun getSortedAlbums(@Query("userId") userId: Int): Response<Albums>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbum(@Path(value = "id") albumId: Int): Response<AlbumItem>
 }
--- app/src/main/java/com/hilbing/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/hilbing/retrofitdemo/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.hilbing.retrofitdemo
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.liveData
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Response
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var retrofitService: AlbumService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val retrofitService = RetrofitInstance
+
+        retrofitService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
+
+        getRequestWithPathParameters()
+        getRequestWithQueryParameter()
+
+
+
+    }
+
+    private fun getRequestWithQueryParameter(){
         val responseLiveData: LiveData<Response<Albums>> = liveData {
             val response = retrofitService.getSortedAlbums(3)
             emit(response)
@@ -31,6 +49,19 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
+        })
+    }
+
+    private fun getRequestWithPathParameters(){
+        //path parameter example
+        val pathResponse: LiveData<Response<AlbumItem>> = liveData {
+            val response = retrofitService.getAlbum(3)
+            emit(response)
+        }
+
+        pathResponse.observe(this, Observer {
+            val title = it.body()?.title
+            Toast.makeText(applicationContext, title, Toast.LENGTH_SHORT).show()
         })
     }
 }
